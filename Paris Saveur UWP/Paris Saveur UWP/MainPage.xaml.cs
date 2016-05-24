70506b2d7875dec6c49a6f3af60787a3f99ed49e
zelong <zelong.chen@applidget.com>
2016-05-24 15:56:02 +0200

    use global command bar style

--- Paris Saveur UWP/Paris Saveur UWP/MainPage.xaml.cs
+++ Paris Saveur UWP/Paris Saveur UWP/MainPage.xaml.cs
@@ -29,7 +29,7 @@ namespace Paris_Saveur_UWP
 
         private void RecommendedGrid_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            Frame.Navigate(typeof(TestPage));
+            Frame.Navigate(typeof(RestaurantListPage));
         }
 
         private void NearByGrid_Tapped(object sender, TappedRoutedEventArgs e)
